1abc60f6da3c6d2f63de887034c12671d82518f5
xiaoshuai96 <1174320669@qq.com>
2020-04-08 22:40:03 +0800

    del out/

--- src/leetCode/demo.java
+++ src/leetCode/demo.java
@@ -1,18 +1,25 @@
 package leetCode;
 
-import java.io.Serializable;
-import java.util.Arrays;
-import java.util.List;
-
 public class demo {
     public static void main(String[] args) {
-        String str = "BEA";
-        new demo().modify(str);
-    }
-    public static void modify(String str){
-        str.replace('A','E');
-        str.toLowerCase();
-        str += 'B';
+        int[][] matrix = new int[][]{
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+        };
+        test(matrix);
     }
 
+
+    public static void test(int[][] matrix){
+
+    }
 }
+class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode(int x) {
+         val = x;
+     }
+ }
