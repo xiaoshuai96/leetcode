be150c088eee1f2d5fe4f7b94e281317c0393348
xiaoshuai96 <1174320669@qq.com>
2020-04-14 15:23:47 +0800

    linkedlist cycle

---
+++ src/leetCode/linkedList/hasCycle_141.java
@@ -0,0 +1,50 @@
+package leetCode.linkedList;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 给定一个链表，判断链表中是否有环。
+ *
+ * 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。
+ * 如果 pos 是 -1，则在该链表中没有环。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：head = [3,2,0,-4], pos = 1
+ * 输出：true
+ * 解释：链表中有一个环，其尾部连接到第二个节点。
+ *
+ * 示例 2：
+ *
+ * 输入：head = [1,2], pos = 0
+ * 输出：true
+ * 解释：链表中有一个环，其尾部连接到第一个节点。
+ *
+ * 示例 3：
+ *
+ * 输入：head = [1], pos = -1
+ * 输出：false
+ * 解释：链表中没有环。
+ *
+ *
+ */
+public class hasCycle_141 {
+    public static void main(String[] args) {
+
+    }
+    public static boolean hashCycle(ListNode head){
+        Set<ListNode> set = new HashSet<>();
+        while (head != null) {
+            if (set.contains(head)) {
+                return true;
+            } else {
+                set.add(head);
+            }
+            head = head.next;
+        }
+        return false;
+    }
+}
