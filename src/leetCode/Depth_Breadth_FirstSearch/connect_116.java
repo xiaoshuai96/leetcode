7dce1a73ca951f920a277e57eaa399020f8e8439
xiaoshuai96 <1174320669@qq.com>
2020-04-22 23:14:38 +0800

    2020-04-22  dfs-116

---
+++ src/leetCode/Depth_Breadth_FirstSearch/connect_116.java
@@ -0,0 +1,61 @@
+package leetCode.Depth_Breadth_FirstSearch;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 给定一个完美二叉树，其所有叶子节点都在同一层，每个父节点都有两个子节点。二叉树定义如下：
+ *
+ * struct Node {
+ *   int val;
+ *   Node *left;
+ *   Node *right;
+ *   Node *next;
+ * }
+ *
+ * 填充它的每个 next 指针，让这个指针指向其下一个右侧节点。
+ * 如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
+ *
+ * 初始状态下，所有 next 指针都被设置为 NULL。
+ *
+ */
+public class connect_116 {
+    class Node{
+        int val;
+        Node left;
+        Node right;
+        Node next;
+    }
+    public static void main(String[] args){
+
+    }
+
+    /**
+     * bfs
+     * @param root
+     * @return
+     */
+    public static Node connect(Node root) {
+        if (root == null) {
+            return root;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                Node node = queue.poll();
+                if (i < size - 1) {
+                    node.next = queue.peek();
+                }
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+        }
+        return root;
+    }
+}
