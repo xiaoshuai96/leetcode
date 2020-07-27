736896ad97a5f82aa47ec910110130ed6c05da5c
xiaoshuai96 <1174320669@qq.com>
2020-07-27 21:46:00 +0800

    2020-07-27 twoPoints 392

---
+++ src/leetCode/twoPointers/isSubsequence_392.java
@@ -0,0 +1,68 @@
+package leetCode.twoPointers;
+
+/**
+ * 给定字符串 s 和 t ，判断 s 是否为 t 的子序列。
+ * 你可以认为 s 和 t 中仅包含英文小写字母。字符串 t 可能会很长（长度 ~= 500,000），
+ * 而 s 是个短字符串（长度 <=100）。
+ * 字符串的一个子序列是原始字符串删除一些（也可以不删除）字符而不改变剩余字符相对位置形成的新字符串。
+ * （例如，"ace"是"abcde"的一个子序列，而"aec"不是）。
+ *
+ * 示例 1:
+ * s = "abc", t = "ahbgdc"
+ * 返回 true.
+ *
+ * 示例 2:
+ * s = "axc", t = "ahbgdc"
+ * 返回 false.
+ *
+ * 后续挑战 :
+ * 如果有大量输入的 S，称作S1, S2, ... , Sk 其中 k >= 10亿，
+ * 你需要依次检查它们是否为 T 的子序列。在这种情况下，你会怎样改变代码？
+ */
+public class isSubsequence_392 {
+    public static void main(String[] args) {
+        String s = "axc",t = "ahbgdc";
+        boolean b = isSubsequence01(s, t);
+        System.out.println(b);
+    }
+
+    /**
+     * 执行用时：1 ms, 在所有 Java 提交中击败了76.11% 的用户
+     * 内存消耗：37.3 MB, 在所有 Java 提交中击败了100.00% 的用户
+     *
+     * twopoints
+     * @param s
+     * @param t
+     * @return
+     */
+    public static boolean isSubsequence01(String s,String t){
+        int slen = s.length(),tlen = t.length();
+        if (slen > tlen) return false;
+        int ls = 0,lt = 0,count = 0;
+        while (ls < slen && lt < tlen) {
+            if (s.charAt(ls) == t.charAt(lt)) {
+                count++;
+                ls++;
+                lt++;
+            } else {
+                lt++;
+            }
+        }
+        return count == slen;
+    }
+
+    /**
+     * 评论区copy
+     * @param s
+     * @param t
+     * @return
+     */
+    public static boolean isSubsequence02(String s,String t){
+        int index = -1;
+        for (char c : s.toCharArray()){
+            index = t.indexOf(c, index+1);
+            if (index == -1) return false;
+        }
+        return true;
+    }
+}
