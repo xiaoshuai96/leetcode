fee1d36a085140d078611b586a5bcb50dfe9f07a
xiaoshuai96 <1174320669@qq.com>
2020-08-09 22:53:56 +0800

    二叉搜索树的恢复-99 and twoPointers-21

---
+++ src/leetCode/tree/recoverTree_99.java
@@ -0,0 +1,168 @@
+package leetCode.tree;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+/**
+ * 二叉搜索树中的两个节点被错误地交换。
+ *
+ * 请在不改变其结构的情况下，恢复这棵树。
+ *
+ * 示例 1:
+ *
+ * 输入: [1,3,null,null,2]
+ *
+ *    1
+ *   /
+ *  3
+ *   \
+ *    2
+ *
+ * 输出: [3,1,null,null,2]
+ *
+ *    3
+ *   /
+ *  1
+ *   \
+ *    2
+ *
+ * 示例 2:
+ *
+ * 输入: [3,1,4,null,null,2]
+ *
+ *   3
+ *  / \
+ * 1   4
+ *    /
+ *   2
+ *
+ * 输出: [2,1,4,null,null,3]
+ *
+ *   2
+ *  / \
+ * 1   4
+ *    /
+ *   3
+ *
+ * 进阶:
+ *
+ *     使用 O(n) 空间复杂度的解法很容易实现。
+ *     你能想出一个只使用常数空间的解决方案吗？
+ */
+public class recoverTree_99 {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     *
+     * 执行用时：4 ms, 在所有 Java 提交中击败了31.16% 的用户
+     * 内存消耗：40.1 MB, 在所有 Java 提交中击败了45.72% 的用户
+     *
+     * 思路：
+     * 二叉搜索树通过中序遍历之后得到的是一串递增的数，
+     * 例如：正常情况下我们得到的是：[1,2,3,4,5,6] 每个数都满足arr[i+1] > arr[i]
+     * 被破坏的树中序遍历的结果：[1,6,3,4,5,2] 但是这里1号位置和5号位置是不符合要求的，这时我们就发现了
+     * 不满足条件的两个节点，我们记录下来他们的值，然后去到被破坏的树中交换他们的位置。
+     * @param root
+     */
+    public void recoverTree01(TreeNode root){
+        List<Integer> list = new ArrayList<>();
+        inorder(root,list);
+        int[] swapped = findTwoSwapped(list);
+        recover(root,2,swapped[0],swapped[1]);
+    }
+
+    /**
+     * 中序遍历树,将遍历到的值都存储到list中
+     */
+    private void inorder(TreeNode root, List<Integer> list){
+        if (root == null) {
+            return ;
+        }
+        inorder(root.left,list);
+        list.add(root.val);
+        inorder(root.right,list);
+    }
+
+    /**
+     * 找到两个不符合条件的值
+     * @param list
+     * @return
+     */
+    private int[] findTwoSwapped(List<Integer> list){
+        int x = -1, y = -1;
+        for (int i = 0; i < list.size() - 1; i++) {
+            //符合条件(如果后一个数字小于前一个数字)
+            if (list.get(i + 1) < list.get(i)) {
+                y = list.get(i + 1);
+                if (x == -1) {
+                    x = list.get(i);
+                } else {
+                    break;
+                }
+            }
+        }
+        return new int[]{x,y};
+    }
+
+    /**
+     *
+     * @param root
+     * @param count 记录次数
+     * @param x
+     * @param y
+     */
+    private void recover(TreeNode root,int count,int x,int y){
+        if (root != null) {
+            if (root.val == x || root.val == y) {
+                //如果当前结点的值以前等于x，现在给换成y；否则反之
+                root.val = root.val == x ? y : x;
+                if (--count == 0) {
+                    return;
+                }
+            }
+            recover(root.left,  count, x, y);
+            recover(root.right, count, x, y);
+        }
+    }
+
+
+    /**
+     * 使用ArrayDeque来进行节点存储
+     *
+     * @param root
+     */
+    public void recoverTree02(TreeNode root){
+        Deque<TreeNode> stack = new ArrayDeque<TreeNode>();
+        TreeNode x = null, y = null, pred = null;
+
+        while (!stack.isEmpty() || root != null) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if (pred != null && root.val < pred.val) {
+                y = root;
+                if (x == null) {
+                    x = pred;
+                } else {
+                    break;
+                }
+            }
+            pred = root;
+            root = root.right;
+        }
+
+        swap(x, y);
+    }
+
+    public void swap(TreeNode x, TreeNode y) {
+        int tmp = x.val;
+        x.val = y.val;
+        y.val = tmp;
+    }
+}
---
+++ src/leetCode/twoPointers/exchange_Offer_21.java
@@ -0,0 +1,79 @@
+package leetCode.twoPointers;
+
+import java.util.Arrays;
+
+/**
+ * 输入一个整数数组，实现一个函数来调整该数组中数字的顺序，使得所有奇数位于数组的前半部分，
+ * 所有偶数位于数组的后半部分。
+ *
+ * 示例：
+ * 输入：nums = [1,2,3,4]
+ * 输出：[1,3,2,4]
+ * 注：[3,1,2,4] 也是正确的答案之一。
+ *
+ * 提示：
+ *
+ *     1 <= nums.length <= 50000
+ *     1 <= nums[i] <= 10000
+ */
+public class exchange_Offer_21 {
+    public static void main(String[] args) {
+        int[] arr = {2,16,3,5,13,1,16,1,12,18,11,8,11,11,5,1};
+        int[] ints = exchange(arr);
+        System.out.println(Arrays.toString(ints));
+    }
+
+    /**
+     * 执行用时：3 ms, 在所有 Java 提交中击败了35.18% 的用户
+     * 内存消耗：47.8 MB, 在所有 Java 提交中击败了54.61% 的用户
+     * @param nums
+     * @return
+     */
+    public static int[] exchange(int[] nums){
+        int left = 0,right = nums.length - 1;
+        while (left < right) {
+            if (nums[left] % 2 == 0 && nums[right] % 2 == 1) {
+                change(left,right,nums);
+                left++;
+                right--;
+            } else if (nums[left] % 2 == 1 && nums[right] % 2 == 1) {
+                left++;
+            } else if (nums[left] % 2 == 0 && nums[right] % 2 == 0) {
+                right--;
+            } else if (nums[left] % 2 == 1 && nums[right] % 2 == 0) {
+                left++;
+                right--;
+            }
+        }
+        return nums;
+    }
+    private static void change(int i,int j,int[] nums){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    /**
+     * 双指针：一个在最前面、一个在最后面，两个小循环分别找到偶数和奇数，然后进行交换
+     * @param nums
+     * @return
+     */
+    public int[] exchange02(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            while (left < right && nums[left] % 2 != 0) {
+                left++;
+            }
+            while (left < right && nums[right] % 2 == 0) {
+                right--;
+            }
+            if (left < right) {
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+            }
+        }
+        return nums;
+    }
+}
