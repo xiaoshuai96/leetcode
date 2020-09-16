2f352ecbc4d559eabe3bb91edb585c43347e3c14
xiaoshuai96 <1174320669@qq.com>
2020-09-16 09:25:47 +0800

    2020-09-16 反转二叉树

--- src/leetCode/tree/invertTree_226.java
+++ src/leetCode/tree/invertTree_226.java
@@ -1,4 +1,7 @@
 package leetCode.tree;
+
+import leetCode.TreeNode;
+
 /**
  * 翻转一棵二叉树。
  *
@@ -25,6 +28,13 @@ public class invertTree_226 {
     public static void main(String[] args) {
 
     }
+
+    /**
+     * 执行用时：0 ms, 在所有 Java 提交中击败了100.00% 的用户
+     * 内存消耗：36.6 MB, 在所有 Java 提交中击败了21.86% 的用户
+     * @param root
+     * @return
+     */
     public TreeNode invertTree(TreeNode root){
         if (root == null) return null;
         TreeNode left = invertTree(root.left);
