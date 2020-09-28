bfaffe80227ad824ea58304c5d734a37dd90e447
xiaoshuai96 <1174320669@qq.com>
2020-09-28 10:20:04 +0800

    144 Iteration dfs

--- src/leetCode/tree/preorderTraversal_144.java
+++ src/leetCode/tree/preorderTraversal_144.java
@@ -1,6 +1,9 @@
 package leetCode.tree;
 
+import leetCode.TreeNode;
+
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Stack;
 
@@ -26,20 +29,29 @@ public class preorderTraversal_144 {
     public static void main(String[] args) {
 
     }
-    public static List<Integer> preTraversal(Node root){
-        List<Integer> list = new ArrayList<>();
+
+    /**
+     * 迭代版本
+     * 执行用时：0 ms, 在所有 Java 提交中击败了100.00% 的用户
+     * 内存消耗：37.5 MB, 在所有 Java 提交中击败了5.08% 的用户
+     * @param root
+     * @return
+     */
+    public static List<Integer> preTraversal01(TreeNode root){
+        List<Integer> list = new LinkedList<>();
         if (root == null) return list;
-        Stack<Node> stack = new Stack<>();
+        LinkedList<TreeNode> stack = new LinkedList<>();
         //首先将根结点压入栈中
         stack.push(root);
         while (!stack.isEmpty()) {
             //取出栈顶的结点
-            Node temp = stack.pop();
+            TreeNode temp = stack.pollLast();
 
             //先将结点的value加入list中
-            list.add(temp.value);
+            list.add(temp.val);
 
             //再判断该节点的左右子树是否为空，如果不为空就将当前结点的左子树或者右子树加入栈中
+            //为什么先遍历右节点，因为栈的特性（先入后出），我们的遍历是前序遍历
             if (temp.right != null) {
                 stack.push(temp.right);
             }
@@ -48,5 +60,29 @@ public class preorderTraversal_144 {
             }
         }
         return list;
+    }
+
+    static List<Integer> res = new ArrayList<>();
+
+    /**
+     * 递归版本
+     * @param root
+     * @return
+     */
+    public static List<Integer> preTraversal02(TreeNode root){
+        dfs(root);
+        return res;
+    }
+    private static void dfs(TreeNode root){
+        if (root == null) {
+            return;
+        }
+        res.add(root.val);
+        if (root.left != null) {
+            dfs(root.left);
+        }
+        if (root.right != null) {
+            dfs(root.right);
+        }
     }
 }
