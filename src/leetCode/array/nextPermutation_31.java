74e5297945555bcb9e156352e88b33a37de81fdb
xiaoshuai96 <1174320669@qq.com>
2020-11-10 10:32:17 +0800

    nextPermutation_31

---
+++ src/leetCode/array/nextPermutation_31.java
@@ -0,0 +1,60 @@
+package leetCode.array;
+/**
+ * 实现获取下一个排列的函数，算法需要将给定数字序列重新排列成字典序中下一个更大的排列。
+ *
+ * 如果不存在下一个更大的排列，则将数字重新排列成最小的排列（即升序排列）。
+ *
+ * 必须原地修改，只允许使用额外常数空间。
+ *
+ * 以下是一些例子，输入位于左侧列，其相应输出位于右侧列。
+ * 1,2,3 → 1,3,2
+ * 3,2,1 → 1,2,3
+ * 1,1,5 → 1,5,1
+ */
+public class nextPermutation_31 {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 执行用时：1 ms, 在所有 Java 提交中击败了98.72% 的用户
+     * 内存消耗：38.6 MB, 在所有 Java 提交中击败了86.72% 的用户
+     * @param nums
+     */
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        //从后往前找出左数小于右数的位置
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (j >= 0 && nums[i] >= nums[j]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1);
+    }
+
+    //交换
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    /**
+     * 反转数组
+     * @param nums
+     * @param start 开始反转的下标
+     */
+    public void reverse(int[] nums, int start) {
+        int left = start, right = nums.length - 1;
+        while (left < right) {
+            swap(nums, left, right);
+            left++;
+            right--;
+        }
+    }
+}
