64b709e10f641e4d8ca7da42149087b574d88703
xiaoshuai96 <1174320669@qq.com>
2021-08-13 19:11:04 +0800

    DP & LinkedList 2021/08/13

---
+++ src/leetCode/linkedList/copyRandomList_138.java
@@ -0,0 +1,60 @@
+package leetCode.linkedList;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 请实现 copyRandomList 函数，复制一个复杂链表。在复杂链表中，每个节点除了有一个 next 指针指向下一个节点，还有一个 random 指针指向链表中的任意节点或者 null。
+ *
+ * 示例 1：
+ * 输入：head = [[7,null],[13,0],[11,4],[10,2],[1,0]]
+ * 输出：[[7,null],[13,0],[11,4],[10,2],[1,0]]
+ *
+ * 示例 2：
+ * 输入：head = [[1,1],[2,1]]
+ * 输出：[[1,1],[2,1]]
+ *
+ * 示例 3：
+ * 输入：head = [[3,null],[3,0],[3,null]]
+ * 输出：[[3,null],[3,0],[3,null]]
+ *
+ * 示例 4：
+ * 输入：head = []
+ * 输出：[]
+ * 解释：给定的链表为空（空指针），因此返回 null。
+ *
+ *
+ * 提示：
+ *     -10000 <= Node.val <= 10000
+ *     Node.random 为空（null）或指向链表中的节点。
+ *     节点数目不超过 1000 。
+ */
+public class copyRandomList_138 {
+    public Node copyRandomList(Node head){
+        if (head == null) return head;
+        Node curr = head;
+        Map<Node, Node> map = new HashMap<>();
+        while (curr != null) {
+            map.put(curr, new Node(curr.val));
+            curr = curr.next;
+        }
+        curr = head;
+        while (curr != null) {
+            map.get(curr).next = map.get(curr.next);
+            map.get(curr).random = map.get(curr.random);
+            curr = curr.next;
+        }
+        return map.get(head);
+    }
+    class Node {
+        int val;
+        Node next;
+        Node random;
+
+        public Node(int val) {
+            this.val = val;
+            this.next = null;
+            this.random = null;
+        }
+    }
+}
